f07e16e272b0b3166f50f421e6872914d416176f
Sumit6893 <abhisonu6893@gmail.com>
2025-07-31 22:39:04 +0530

    initial commit

---
+++ backend/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const eventRoute = require("./controller/eventRoute");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+dotenv.config();
+
+
+mongoose.set("strictQuery", true);
+mongoose.connect(process.env.MONGOURL);
+var db = mongoose.connection;
+db.on("open", () => console.log("Connected to DB"));
+db.on("error", () => console.log("Error occurred"));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(cors());
+
+app.use('/eventRoute', eventRoute);
+app.listen(4000, () => {
+    console.log("Server started at 4000");
+})
